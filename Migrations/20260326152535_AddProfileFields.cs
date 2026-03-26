886a492609fa5224685643f81431ba60e44d3c48
vanlinh <vanlinh6199@gmail.com>
2026-03-26 22:59:06 +0700

    Thêm thông tin tài khoản, datatable cho phòng

---
+++ Migrations/20260326152535_AddProfileFields.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DoAnWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProfileFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Phone",
+                table: "AspNetUsers",
+                newName: "UserPhone");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AvatarUrl",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AvatarUrl",
+                table: "AspNetUsers");
+
+            migrationBuilder.RenameColumn(
+                name: "UserPhone",
+                table: "AspNetUsers",
+                newName: "Phone");
+        }
+    }
+}
